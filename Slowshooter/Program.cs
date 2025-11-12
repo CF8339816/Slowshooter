920acb9e37f34c58643533477d203e530305008a
CF8339816 <chris_french_3dga@hotmail.com>
2025-11-12 15:46:25 -0400

    game functional
    fixed pickups and adjusted colour display and the win conditions

--- Slowshooter/Program.cs
+++ Slowshooter/Program.cs
@@ -3,6 +3,12 @@ using System.Linq;
 
 namespace Slowshooter
 {
+
+    /*Inital code Simon Forsythe
+     * Additinal code  Aiden Buzsik, Chris French
+     */
+
+
     internal class Program
     {
 
@@ -14,8 +20,8 @@ namespace Slowshooter
 +-----+   +-----+";
 
         static bool isPlaying = true;
-
-        #region Player 1 Pickup Random
+//Defining Player 1 pickup random A.B.
+        #region Player 1 Pickup Random   
 
         //player 1 pickup X and Y random
         static Random Player1PickUpsXRnD = new Random();
@@ -26,7 +32,7 @@ namespace Slowshooter
         static int pickup1_y_pos;
 
         #endregion
-
+        //Defining Player 2 pickup random A.B.
         #region Player 2 Pickup
 
         //player 2 pickup X and Y random
@@ -44,6 +50,11 @@ namespace Slowshooter
 
         static (int, int) player1_position;
         static (int, int) player2_position;
+
+        static (int, int) player1_positionPROXY; //Initalizing Player 1 Proxy turple for pickup compairason C.F.
+        static (int, int) player2_positionPROXY; //Initalizing Player 2 Proxy turple for pickup compairason C.F.
+
+
 
         static int p1_x_input;
         static int p1_y_input;
@@ -133,7 +144,7 @@ namespace Slowshooter
             // update players' positions based on input
             p1_x_pos += p1_x_input;
             p1_x_pos = p1_x_pos.Clamp(p1_min_max_x.Item1, p1_min_max_x.Item2);
-
+                    
             p1_y_pos += p1_y_input;
             p1_y_pos = p1_y_pos.Clamp(p1_min_max_y.Item1, p1_min_max_y.Item2);
 
@@ -146,13 +157,19 @@ namespace Slowshooter
             player1_position = (p1_y_pos, p1_x_pos);
             player2_position = (p2_y_pos, p2_x_pos);
 
-            if(player1_position == pickup1_position)
-            {
+            //defining ProxyPlayer Position for the compairason to pickup location to reverse the turple for compairason. C.F.
+            player1_positionPROXY = (p1_x_pos, p1_y_pos);
+            player2_positionPROXY = (p2_x_pos, p2_y_pos);
+
+            if (player1_positionPROXY == pickup1_position) //Redefining the pickup conditions for points  C.F.
+            //if (player1_position == pickup1_position)
+                {
                 player1_score++;
                 generatePickup1 = true;
             }
 
-            if (player2_position == pickup2_position)
+            if (player2_positionPROXY == pickup2_position) //Redefining the pickup conditions for points  C.F.
+            //if (player2_position == pickup2_position)
             {
                 player2_score++;
                 generatePickup2 = true;
@@ -181,19 +198,20 @@ namespace Slowshooter
             Console.ForegroundColor = playerColors[1];
             Console.Write("O");
 
-            //draw pickups
+            //draw pickups  A.B.
             if(generatePickup1)
             {
+               
                 generatePickup1 = false;
                 pickup1_x_pos = Player1PickUpsXRnD.Next(1, 6);
                 pickup1_y_pos = Player1PickUpsYRnD.Next(1, 4);
                 pickup1_position = (pickup1_x_pos, pickup1_y_pos);
             }
            
-            Console.ForegroundColor = playerColors[0]; //uses predefined player1 color
+            Console.ForegroundColor = playerColors[0]; //uses predefined player1 color C.F.
             //Console.ForegroundColor = ConsoleColor.Red;
             Console.SetCursorPosition(pickup1_position.Item1, pickup1_position.Item2);
-            Console.Write("*"); // player write function uses write not writeline, trying it to see if it makes a difference with the pickup issues ww were having
+            Console.Write("*"); // player write function uses write not writeline, trying it to see if it makes a difference with the pickup issues ww were having C.F.
             //Console.WriteLine("*");
 
             if (generatePickup2)
@@ -205,13 +223,13 @@ namespace Slowshooter
             }
 
 
-            Console.ForegroundColor = playerColors[1]; //uses predefined player2 color
+            Console.ForegroundColor = playerColors[1]; //uses predefined player2 color  C.F.
             //Console.ForegroundColor = ConsoleColor.Blue;
             Console.SetCursorPosition(pickup2_position.Item1, pickup2_position.Item2);
-            Console.Write("*"); // player write function uses write not writeline, trying it to see if it makes a difference with the pickup issues ww were having
-           //Console.WriteLine("*");
+            Console.Write("*"); // player write function uses write not writeline, trying it to see if it makes a difference with the pickup issues ww were having C.F.
+            //Console.WriteLine("*");
 
-            // draw the Turn Indicator
+            // draw the Turn Indicator  Modified  to better define key usage  A.B.
             Console.SetCursorPosition(0, 5);
             Console.ForegroundColor = playerColors[turn % 2];
 
@@ -219,47 +237,92 @@ namespace Slowshooter
 
 
             Console.ForegroundColor = ConsoleColor.DarkGray;
-            Console.Write(" To move please use:");
+            Console.Write(" To move please use:");// reworked the message text to better use predefined player2 color  C.F.
 
             if (turn % 2 == 0)
             {
-                Console.ForegroundColor = playerColors[0];
+                Console.ForegroundColor = playerColors[0]; //added this code to better use predefined player1 color  C.F.
                 Console.WriteLine("\n WASD");
             }
             else
             {
-                Console.ForegroundColor = playerColors[1];
+                Console.ForegroundColor = playerColors[1]; //added this code to better use predefined player2 color  C.F.
                 Console.WriteLine("\n IJKL");
             }
+            //Coded the player score and pickup location debugs below A.B
+            //tweaked the colour s to reflect player colours and added Player location and proxy location debgs C.F.
+
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("debug block\n");
 
             Console.ForegroundColor = ConsoleColor.White;
-
             Console.Write("Player 1 Score:");
             Console.ForegroundColor = playerColors[0];
             Console.WriteLine(player1_score);
+
             Console.ForegroundColor = ConsoleColor.White;
             Console.Write("Player 1 Pickup:");
             Console.ForegroundColor = playerColors[0];
             Console.WriteLine(pickup1_position);
+            
+            
+            //Found out after adding player position for debugging that the positions display   y, x for  pickup and  x,y for player so not getting the correct location C.F. 
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("Player 1 position:");
+            Console.ForegroundColor = playerColors[0];
+            Console.WriteLine(player1_position);
+            //checking the new proxy for position compliance C.F.
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("Player 1 position:");
+            Console.ForegroundColor = playerColors[0];
+            Console.WriteLine(player1_positionPROXY);
+
+
+
             Console.ForegroundColor = ConsoleColor.White;
             Console.Write("Player 2 Score:");
             Console.ForegroundColor = playerColors[1];
             Console.WriteLine(player2_score);
+
             Console.ForegroundColor = ConsoleColor.White;
             Console.Write("Player 2 Pickup:");
             Console.ForegroundColor = playerColors[1];
             Console.WriteLine(pickup2_position);
-            Console.ForegroundColor = ConsoleColor.White;
-           
-            //if (player1_score == 5)
-            //{
-            //    Console.WriteLine($" Player 1 has reached 5 points. Player 1 wins.");
-            //}
 
-            //if (player2_score == 5)
-            //{
-            //    Console.WriteLine($" Player 2 has reached 5 points. Player 1 wins.");
-            //}
+
+            //Found out after adding player position for debugging that the positions display   y, x for  pickup and  x,y for player so not getting the correct location C.F. 
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("Player 2 position:");
+            Console.ForegroundColor = playerColors[1];
+            Console.WriteLine(player2_position);
+            //checking the new proxy for position compliance C.F.
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("Player 2 position:");
+            Console.ForegroundColor = playerColors[1];
+            Console.WriteLine(player2_positionPROXY);
+            Console.WriteLine();
+            Console.WriteLine();
+
+
+
+
+            //Defining winn conditions and message.
+
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            if (player1_score == 5)
+            {
+                Console.WriteLine($" Player 1 has reached 5 points. Player 1 wins.");
+               
+            }
+
+            if (player2_score == 5)
+            {
+                Console.WriteLine($" Player 2 has reached 5 points. Player 2 wins.");
+               
+            }
+            Console.ForegroundColor = ConsoleColor.White;
 
         }
     }
